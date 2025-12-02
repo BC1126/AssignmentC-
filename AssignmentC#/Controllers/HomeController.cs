924521792faf5e8e0c17ce14b7f61b1d8c4c09f3
Saw0818 <sawjs-pm23@student.tarc.edu.my>
2025-12-03 03:47:32 +0800

    routing

--- AssignmentC#/Controllers/HomeController.cs
+++ AssignmentC#/Controllers/HomeController.cs
@@ -1,12 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace AssignmentC_.Controllers
+namespace AssignmentC_.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
+
+    public IActionResult movie()
+    {
+        return View();
+    }
+
+    public IActionResult detail()
+    {
+        return View();
+    }
+
+    public IActionResult login()
+    {
+        return View();
+    }
+
+    public IActionResult register()
+    {
+        return View();
+    }
+
+    public IActionResult blog()
+    {
+        return View();
+    }
+
+    public IActionResult blogDetail()
+    {
+        return View();
+    }
+
+    public IActionResult ticket()
+    {
+        return View();
+    }
+
+    public IActionResult about()
+    {
+        return View();
+    }
+
+    public IActionResult contact()
+    {
+        return View();
+    }
+
+    public IActionResult faq()
+    {
+        return View();
     }
 }
