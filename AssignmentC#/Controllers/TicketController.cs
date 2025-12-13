88179363e118bec3c95b095787183e1e7254c08f
Saw0818 <sawjs-pm23@student.tarc.edu.my>
2025-12-14 02:44:35 +0800

    payment

---
+++ AssignmentC#/Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentC_.Controllers;
+
+public class TicketController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    public IActionResult Purchase()
+    {
+        return View();
+    }
+}
