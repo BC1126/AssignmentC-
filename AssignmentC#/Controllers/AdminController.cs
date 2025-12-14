8dd18d200da72fda234ea6c6490eade8adffdaf8
Danial-Irfan6k <muhammaddib-pm23@student.tarc.edu.my>
2025-12-14 16:33:58 +0800

    small

--- AssignmentC#/Controllers/AdminController.cs
+++ AssignmentC#/Controllers/AdminController.cs
@@ -238,8 +238,19 @@ public class AdminController : Controller
 
     }
 
-}
-
-
-
+}        return View();
+    }
+    public IActionResult AddHall()
+    {
+        return View();
+    }
+}    public IActionResult AddHall()
+    {
+        return View();
+    }
+}    }
+    public IActionResult AddHall()
+    {
+        return View();
+    }
 }
