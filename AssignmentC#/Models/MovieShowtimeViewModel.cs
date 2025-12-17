0b2ec7991ddcc1c4413ff56a61ac8cc61a18ec04
Danial-Irfan6k <muhammaddib-pm23@student.tarc.edu.my>
2025-12-17 12:29:33 +0800

    add

---
+++ AssignmentC#/Models/MovieShowtimeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AssignmentC_.Models;
+
+namespace AssignmentC_.Models;
+
+public class MovieShowtimeViewModel
+{
+    public Movie Movie { get; set; }
+    public List<OutletShowtimes> GroupedShowtimes { get; set; } = new();
+}
+
+public class OutletShowtimes
+{
+    public int OutletId { get; set; }
+    public string OutletName { get; set; }
+    public string City { get; set; }
+    public List<ShowtimeInfo> Showtimes { get; set; } = new();
+}
+
+public class ShowtimeInfo
+{
+    public int ShowTimeId { get; set; }
+    public DateTime StartTime { get; set; }
+    public decimal TicketPrice { get; set; }
+    public string HallName { get; set; }
+    public string HallType { get; set; }
+    public int AvailableSeats { get; set; }
+}
